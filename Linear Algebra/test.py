55a25334a156bac9be323a747d53a17dfa235952
Zhongguan(Ryan) Wen <zgwen1217@gmail.com>
2025-08-25 21:00:23 +0800

    20250825

--- Linear Algebra/test.py
+++ Linear Algebra/test.py
@@ -1,14 +1,21 @@
 import numpy as np
 
-A = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype=np.dtype(float))
+# test 1
+# A = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype=np.dtype(float))
+#
+# b = np.array([1, 1, 1], dtype=np.dtype(float))
+#
+# print(A)
+# print(b)
+#
+# x = np.linalg.solve(A, b)
+# print(x)
+#
+# d = np.linalg.det(A)
+# print(d)
 
-b = np.array([1, 1, 1], dtype=np.dtype(float))
 
-print(A)
-print(b)
+# test 2
 
-x = np.linalg.solve(A, b)
-print(x)
-
-d = np.linalg.det(A)
-print(d)
+for j in range(1, 3):
+    print(j)
