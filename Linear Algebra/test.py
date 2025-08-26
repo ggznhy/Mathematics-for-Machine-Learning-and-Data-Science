72811f255efcb2c1ef305e1024a78274f079e34e
Zhongguan(Ryan) Wen <zgwen1217@gmail.com>
2025-08-27 02:05:04 +0800

    principal component analysis

--- Linear Algebra/test.py
+++ Linear Algebra/test.py
@@ -17,5 +17,27 @@ import numpy as np
 
 # test 2
 
-for j in range(1, 3):
-    print(j)
+# for j in range(1, 3):
+#     print(j)
+
+
+# test 3
+# Y = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype=np.dtype(float))
+# print(Y)
+# mean_vector = np.mean(Y, axis=0)
+# print(mean_vector)
+# mean_matrix = np.repeat(mean_vector, repeats=Y.shape[0])
+# # use np.reshape to reshape into a matrix with the same size as Y. Remember to use order='F'
+# mean_matrix = np.reshape(mean_matrix, order='F', newshape=Y.shape)
+# print(mean_matrix)
+#
+# X = Y - mean_matrix
+# print(X)
+
+
+# test 4
+vector = np.array([[1,1],[0,1],[1,0]], dtype=np.dtype(float))
+tmp = vector[0:2]
+print(tmp)
+V = tmp/np.linalg.norm(tmp)
+print(V)
